19242d45a43b1c75f5d43e0ca0a96a6d3101851a
Lovish Duggal <lovishduggal11@gmail.com>
2024-05-15 08:28:14 +0530

    add self endpoint

--- src/http/api.ts
+++ src/http/api.ts
@@ -3,4 +3,5 @@ import { api } from './client';
 
 // Auth service:
 export const login = (credentials: Credentials) =>
-    api.post('/auth/login', credentials);
+    api.post('api/auth/login', credentials);
+export const self = () => api.get('api/auth/self');
--- src/pages/Login/login.tsx
+++ src/pages/Login/login.tsx
@@ -11,20 +11,32 @@ import {
 } from 'antd';
 import { LockFilled, LockOutlined, UserOutlined } from '@ant-design/icons';
 import Logo from '../../icons/Logo';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 import { Credentials } from '../../types';
-import { login } from '../../http/api';
+import { login, self } from '../../http/api';
 
 const loginUser = async (credentials: Credentials) => {
     const { data } = await login(credentials);
     return data;
 };
 
+const getSelf = async () => {
+    const { data } = await self();
+    return data;
+};
+
 const LoginPage = () => {
+    const { data: selfData, refetch } = useQuery({
+        queryKey: ['self'],
+        queryFn: getSelf,
+        enabled: false,
+    });
     const { mutate, isPending, isError, error } = useMutation({
         mutationKey: ['login'],
         mutationFn: loginUser,
         onSuccess: async () => {
+            refetch();
+            console.log(selfData);
             console.log('Login successfully');
         },
     });
