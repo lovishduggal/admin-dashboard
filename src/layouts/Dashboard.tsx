2bb39b16f28b23a66912f94fe8557991023639ec
Lovish Duggal <lovishduggal11@gmail.com>
2024-05-16 07:48:28 +0530

    add protected routes

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -1,6 +1,9 @@
-import { Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
+import { useAuthStore } from '../store';
 
 export const Dashboard = () => {
+    const { user } = useAuthStore();
+    if (user == null) return <Navigate to="/auth/login" replace={true} />;
     return (
         <div>
             <h1> Dashboard</h1>
